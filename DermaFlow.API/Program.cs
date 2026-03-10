0a2dbb10df4de8fedb9931a38f65bc3c6d31a644
marcmach1 <bylykoh@gmail.com>
2026-03-10 04:52:55 -0300

    Criacao da pagina de cadastro de usuario

--- DermaFlow.API/Program.cs
+++ DermaFlow.API/Program.cs
@@ -5,28 +5,40 @@ using DermaFlow.Shared.DTOs;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("PermitirBlazor", policy => 
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddMediatR(cfg => 
 {
-    cfg.RegisterServicesFromAssembly(typeof(DermaFlow.Application.Users.Commands.CreateUserCommand).Assembly);
+    cfg.RegisterServicesFromAssembly(typeof(CreateUserCommand).Assembly);
 });
 
 var app = builder.Build();
 
+
+app.UseCors("PermitirBlazor");
+
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.MapPost("/users", async (UserRequestDTO request, IMediator mediator) =>
+{
+    var command = new CreateUserCommand(request.Name, request.Email, request.Password);
+    var userId = await mediator.Send(command);
+    
+    return Results.Created($"/users/{userId}", new { id = userId });
+});
 
-    app.MapPost("/users", async (UserRequestDTO request, IMediator mediator) =>
-    {
-        var command = new CreateUserCommand(request.Name, request.Email, request.Password);
-        var userId = await mediator.Send(command);
-        
-        return Results.Created($"/users/{userId}", new { id = userId });
-    });
 
-app.Run();
+app.Run("http://localhost:5253");
